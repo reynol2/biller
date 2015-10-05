96e4e717d07edbbccabba2b866cdbd7a70cf3b37
Dustin Reynolds <reynol2@pdx.edu>
2015-10-04 22:36:53 -0700

    Add custom validaiton to accounts

--- biller_downloaded/app/models/account.rb
+++ biller_downloaded/app/models/account.rb
@@ -2,6 +2,21 @@ class Account < ActiveRecord::Base
   has_many :account_entries
 
   validates :name, presence: true, 
-                   length: {in: 1..70},
+                   length: {in: 1..70,
+                            message: "Please use a decent name, sir."},
                    uniqueness: true
+  validate :your_name_is_not_dumb
+  validate :name_is_not_length_42
+
+  def your_name_is_not_dumb
+    if name.include?("dumb")
+      errors.add(:name, "is dumb")
+    end
+  end
+
+  def name_is_not_length_42
+    if name.length == 42
+      errors.add(:name, "cannot be 42 characters long or world implodes")
+    end
+  end
 end
